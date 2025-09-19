06323c3133d36f32af8d2df0dc32aece2cef24c6
nugrahazikry <zikriaj@gmail.com>
2025-09-19 23:41:49 +0800

    add additional configuration

--- config/constants.py
+++ config/constants.py
@@ -1,9 +1,11 @@
 import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
+from google import genai
 load_dotenv("environment.env")
 
 GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
 MODEL_NAME = "gemini-2.0-flash-lite"
+CLIENT = genai.Client(api_key=GEMINI_API_KEY)
 
 llm = ChatGoogleGenerativeAI(model=MODEL_NAME, temperature=0, google_api_key=GEMINI_API_KEY)
